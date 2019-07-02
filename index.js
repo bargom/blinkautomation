e69e34748091fab110243cdbe68cd5b4d499421f
Baris Gomleksizoglu <baris.gomleksizoglu@external.toyota-europe.com>
2019-07-02 14:59:07 +0200

    email sending added

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-"use strict";
+'use strict';
 
-const BlinkApi = require('node-blink-security');
+console.log('Loading lambda function...');
 
-class Blink {
-    constructor() {
+const Blink = require('./lib/Blink');
 
+exports.takePhoto = async (event, context, callback) => {
+    try {
+        console.log(`takePhoto event: ${JSON.stringify(event)}`);
+        const blink = new Blink();
+        let buffer = await blink.takePhoto(event.token, event.network, event.camera);
+        //let result = await blink.uploadToS3(buffer);
+        //console.log(result);
+        let emailResult = await blink.sendEmail(buffer, event.email, `Image from Blink for ${event.network}/${event.camera}`, `${event.camera}.jpg`);
+        console.log(emailResult);
+        return callback(null, emailResult);
+    } catch (err) {
+        return callback(err);
     }
-
-    async takePhoto(token, network, camera) {
-        const blinkApi = new BlinkApi("none", "none", {
-            _token: token,
-            _network_id: network,
-            _region_id: "prde" //prde for eu, prod for other
-        });
-        return blinkApi.setupSystem(network)
-            .then(() => {
-                //console.dir(blinkApi);
-                let cameras = blinkApi.cameras;
-                console.log(`These are the cameras: ${JSON.stringify(cameras)}`);
-                let myCamera = cameras[Object.values(cameras).find(c=>c._name===camera)._id];
-                let snapResult = myCamera.snapPicture();
-                console.log(snapResult);
-                //return myCamera.fetchImageData();
-                return myCamera.thumbnail;
-            }, (error) => {
-                console.log(error);
-            });
-    }
-}
-
-module.exports = Blink;
+};
