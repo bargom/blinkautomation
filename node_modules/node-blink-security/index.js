1649b7b03ec764d6fa033e22973313202ea352a1
Baris Gomleksizoglu <baris.gomleksizoglu@external.toyota-europe.com>
2019-07-02 11:36:37 +0200

    removed request

--- node_modules/node-blink-security/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by madshall on 3/17/17.
- */
-
-const Blink = require('./lib/blink');
-
-module.exports = Blink;
--- node_modules/node-blink-security/lib/blink_exception.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by madshall on 3/17/17.
- */
-
-module.exports = class BlinkException extends Error {
-  constructor(message) {
-    super(message);
-  }
-};
